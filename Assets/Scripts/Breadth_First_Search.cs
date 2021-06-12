e64a31d5e94caf35988148decd66c907c2b8cde0
dimitra <dgazepidi@gmail.com>
2021-06-12 17:53:08 +0300

    Basic UI & RayCast hit checks
    Check for existing block above another one

---
+++ Assets/Scripts/Breadth_First_Search.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breadth_First_Search
+{
+    public static bool Clear(GameObject gameObject)
+    {
+        //return Physics.Raycast(gameObject.transform.position, gameObject.transform.TransformDirection(Vector3.up));
+
+        RaycastHit hit;
+        var ray = new Ray(gameObject.transform.position, gameObject.transform.TransformDirection(Vector3.up));
+        if (Physics.Raycast(ray, out hit, 25))
+        {
+            Debug.Log(hit.collider.name);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager game;
+    public GameObject A;
+    public GameObject B;
+    public GameObject C;
+    public GameObject menu;
+
+    //gets called before application starts
+    private void Awake()
+    {
+        if (game == null)
+        {
+            game = this;
+        }
+        else if (game != this) {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //OnClick method
+    public void BS_Start()
+    {
+        Debug.Log("Breadth first search started...");
+        menu.SetActive(false);
+        Debug.Log(Breadth_First_Search.Clear(A));
+        Debug.Log(Breadth_First_Search.Clear(B));
+    }
+
+    //OnClick method
+    public void AS_Start()
+    {
+        Debug.Log("A* search started...");
+        menu.SetActive(false);
+    }
+}
